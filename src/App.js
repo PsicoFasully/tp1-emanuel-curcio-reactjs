a1e74a96a90effbc94da49abb66a21cbde677d58
Emanuel Curcio <emanuelalejandro2021@hotmail.com>
2022-08-23 12:33:29 -0300

    tp1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          editar <code>src/App.js</code> guardar y recargar.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          leer mas sobre reactjs
         </a>
       </header>
     </div>
