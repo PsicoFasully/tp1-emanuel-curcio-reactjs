a33c85159c0442a009bc7c3542cbbc675c1afa4e
Emanuel Curcio <emanuelalejandro2021@hotmail.com>
2022-09-08 22:01:06 -0300

    tp5

--- src/componestes NavBar/ItemListContainer/index.jsx
+++ src/componestes NavBar/ItemListContainer/index.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import ItemCount from '../ItemCount'
+import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { products } from '../data/products';
+import ItemList from '../components/ItemList';
 
+const ItemListContainer = ({ greeting }) => {
 
-export const Ad = ({children}) => {
+    const [productos, setProductos] = useState([])
 
-    const agregarAlCarrito = (cantidad) =>{
-        console.log(`se agrego al carrito la "${cantidad}"al carrito`)
-    }
+    useEffect(() => {
+
+        (async () => {
+            const obtenerProductos = new Promise((accept, reject) => {
+                setTimeout(() => {
+                    accept(products)
+                }, 3000);
+            })
+
+          
+            try {
+                const productos = await obtenerProductos;
+                setProductos(productos);
+            } catch (error) {
+                console.log(error);
+            }
+
+        })()
+
+    }, [])
+
+    console.log(productos)
 
     return (
-        <div>
-
-        {children}
-        <ItemCount initial={1} stock={5} onAdd={agregarAlCarrito}/>
+        <div className='item-list-container'>
+            <ItemList products={productos} />
         </div>
     )
 }
 
-export default Ad
+export default ItemListContainer;
