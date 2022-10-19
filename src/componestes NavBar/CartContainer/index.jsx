e6d799058b60e9becae8714ef517d5721b65a167
Emanuel Curcio <emanuelalejandro2021@hotmail.com>
2022-10-19 11:47:03 -0300

    proyecto final

--- src/componestes NavBar/CartContainer/index.jsx
+++ src/componestes NavBar/CartContainer/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { useContext } from 'react';
-import {shop} from "../../context/ShopProvider"
+
+
 const Cart = () => {
     
-    const {cart, removeItem, clearCart} = useContext(shop)
-    console.log(cart)
-    
+
     return (
         <div>Cart</div>
     )
